4cda35b199d2606f4203eb68cf8cb47b62b94fee
sp1 <sp1@meitu.com>
2023-11-15 20:15:32 +0800

    代码提交

--- test/demo4.py
+++ test/demo4.py
@@ -21,11 +21,18 @@ class MyApp(QtWidgets.QDialog):
 
         time.sleep(3)
         # 安卓投屏相关服务
+        self.phone_show.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        # I'm assuming the following...
+        self.phone_show.setScaledContents(True)
+        # self.phone_show.setFixedSize(701, 451)
+
+        # install an event filter to "capture" mouse events (amongst others)
+        self.phone_show.installEventFilter(self)
+
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_screen)
         self.timer.start(500)
-        self.phone_show.setAttribute(Qt.WA_TransparentForMouseEvents)
-        self.phone_show.show()
+
 
     def update_screen(self):
         img = self.dev.snapshot(quality=100)
@@ -40,6 +47,52 @@ class MyApp(QtWidgets.QDialog):
         scaled_pixmap = pixmap.scaled(self.phone_show.width(), self.phone_show.height(),
                                       QtCore.Qt.AspectRatioMode.KeepAspectRatio)  # 缩放图像以适应 QLabel 大小
         self.phone_show.setPixmap(scaled_pixmap)  # 在 QLabel 上显示缩放后的图像
+
+    def eventFilter(self, source, event):
+        if (source == self.phone_show and source.pixmap() and not source.pixmap().isNull() and
+            event.type() == QtCore.QEvent.MouseButtonPress and
+            event.button() == QtCore.Qt.LeftButton):
+                self.getClickedPosition(event.pos())
+        return super().eventFilter(source, event)
+
+    def getClickedPosition(self, pos):
+        contentsRect = QtCore.QRectF(self.phone_show.contentsRect())
+        if pos not in contentsRect:
+            return
+
+        # adjust the position to the contents margins
+        pos -= contentsRect.topLeft()
+
+        pixmapRect = self.phone_show.pixmap().rect()
+        if self.phone_show.hasScaledContents():
+            x = pos.x() * pixmapRect.width() / contentsRect.width()
+            y = pos.y() * pixmapRect.height() / contentsRect.height()
+            pos = QtCore.QPoint(x, y)
+        else:
+            align = self.phone_show.alignment()
+            pixmapRect = QtCore.QRectF(pixmapRect)
+            if align & QtCore.Qt.AlignRight:
+                pixmapRect.moveRight(contentsRect.x() + contentsRect.width())
+            elif align & QtCore.Qt.AlignHCenter:
+                pixmapRect.moveLeft(contentsRect.center().x() - pixmapRect.width() / 2)
+            # the pixmap is not top aligned (note that the default for QLabel is
+            # Qt.AlignVCenter, the vertical center)
+            if align & QtCore.Qt.AlignBottom:
+                pixmapRect.moveBottom(contentsRect.y() + contentsRect.height())
+            elif align & QtCore.Qt.AlignVCenter:
+                pixmapRect.moveTop(contentsRect.center().y() - pixmapRect.height() / 2)
+
+            if not pos in pixmapRect:
+                # outside image margins, ignore!
+                return
+            # translate coordinates to the image position and convert it back to
+            # a QPoint, which is integer based
+            pos = (pos - pixmapRect.topLeft()).toPoint()
+        x_val=pos.x()
+        y_val=pos.y()
+        os.system(f"adb shell input tap {x_val} {y_val}")
+        # print('X={}, Y={}'.format(pos.x(), pos.y()))
+
 
 
 class MyLabel(ClickableLabel):
