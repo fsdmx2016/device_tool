a463619f8ee52eaeadae7f58476d3b22e5a33410
孙鹏 <sp1@meitu.com>
2023-12-17 11:48:06 +0800

    支持录制、重放

--- Android/app/app_retry.py
+++ Android/app/app_retry.py
@@ -7,6 +7,8 @@ from PyQt5.QtWidgets import QCheckBox, QListWidgetItem, QMessageBox
 
 from Android.app.app_performance import Appa_Performance
 
+select_script = ""
+
 
 class AppRetry:
     def __init__(self, dev):
@@ -14,14 +16,16 @@ class AppRetry:
         self.dev = dev
 
     def run_script_by_name(self, retry_script_list, retry_num, cpu_layout, mem_layout):
-        run_script_thread = threading.Thread(target=self.run_script_(retry_script_list, retry_num))
-        app_performance = Appa_Performance(self.dev, cpu_layout, mem_layout)
-        package_name = str(self.dev.get_top_activity_name()).split("/")[0]
-        thread1 = threading.Thread(target=app_performance.make_cpu_canvas(cpu_layout, package_name))
-        thread2 = threading.Thread(target=app_performance.make_mem_canvas(cpu_layout, package_name))
-        run_script_thread.start()
-        thread1.start()
-        thread2.start()
+        is_has_select=self.is_select_script(retry_script_list)
+        if is_has_select:
+            run_script_thread = threading.Thread(target=self.run_script_(retry_script_list, retry_num))
+            app_performance = Appa_Performance(self.dev, cpu_layout, mem_layout)
+            package_name = str(self.dev.get_top_activity_name()).split("/")[0]
+            thread1 = threading.Thread(target=app_performance.make_cpu_canvas(cpu_layout, package_name))
+            thread2 = threading.Thread(target=app_performance.make_mem_canvas(cpu_layout, package_name))
+            run_script_thread.start()
+            thread1.start()
+            thread2.start()
 
     def delete_script(self, retry_script_list):
         delete_count = 0
@@ -40,15 +44,33 @@ class AppRetry:
             messageBox.button(QMessageBox.Ok)
             messageBox.exec()
 
+
     def get_select_script(self, retry_script_list):
         for i in range(retry_script_list.count()):
             item = retry_script_list.item(i)
             check_box = retry_script_list.itemWidget(item)
             if check_box.isChecked():
                 return check_box.text()
-        messageBox = QMessageBox(QMessageBox.Information, "提示", "请先选择要执行的脚本")
-        messageBox.button(QMessageBox.Ok)
-        messageBox.exec()
+
+    def is_select_script(self, retry_script_list):
+        select_count = 0
+        for i in range(retry_script_list.count()):
+            item = retry_script_list.item(i)
+            check_box = retry_script_list.itemWidget(item)
+            if check_box.isChecked():
+                select_count = select_count + 1
+        if select_count == 1:
+            return True
+        elif select_count > 1:
+            messageBox = QMessageBox(QMessageBox.Information, "提示", "每次只能执行一个")
+            messageBox.button(QMessageBox.Ok)
+            messageBox.exec()
+            return False
+        elif select_count==0:
+            messageBox = QMessageBox(QMessageBox.Information, "提示", "请先选择要执行的脚本")
+            messageBox.button(QMessageBox.Ok)
+            messageBox.exec()
+            return False
 
     def run_script_(self, retry_script_list, retry_num):
         script_name = self.get_select_script(retry_script_list)
